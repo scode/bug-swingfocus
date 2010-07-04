46f70c7423c343f6d165a49338ef124e452b96da
Peter Schuller <peter.schuller@infidyne.com>
2010-07-05 01:41:31 +0200

    Forgotten bits of last commit.

--- src/main/java/org.scode.bug_swingfocus/TestFrame.java
+++ src/main/java/org.scode.bug_swingfocus/TestFrame.java
@@ -6,6 +6,7 @@ import javax.swing.*;
  */
 public class TestFrame extends JFrame {
 	public TestFrame() {
+		addWindowListener(new LoggingWindowListener());
 		JTextField tf = new JTextField();
 		add(tf);
 	}
